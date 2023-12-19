d4cd0f2bc2375f71f4f80faf3ee5cdc8fbcd9e7d
BhavikaThawani <bhavika.t@simformsolutions.com>
2023-12-19 12:20:17 +0530

    js learning

---
+++ script.js
@@ -0,0 +1,15 @@
+// strings and template literals
+// let job = "engineer";
+// let firstName = "bhavika";
+// let description = `I am ${firstName} and my profession is ${job}`;
+// console.log(description);
+
+// if/else statements
+const age = 14;
+const able = age >= 18;
+if (able) {
+  console.log("he/She can drive 😁");
+} else {
+  let yearsRemaining = 18 - age;
+  console.log(`still ${yearsRemaining} years are left 😕`);
+}
