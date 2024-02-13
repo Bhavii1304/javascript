66df331fc4d8443cf62cf35dcbe7a4e3ccfa00e5
BhavikaThawani <bhavika.t@simformsolutions.com>
2024-02-13 19:09:46 +0530

    promise api

--- Section16/section16.js
+++ Section16/section16.js
@@ -257,7 +257,7 @@ const getJson = function (url, errorMessage = "something went wrong") {
 
 // let currentImg;
 
-// createImage("/Section11/img-1.jpg")
+// createImage("/Section16/img-1.jpg")
 //   .then((img) => {
 //     currentImg = img;
 //     console.log("Image 1 loaded");
@@ -265,7 +265,7 @@ const getJson = function (url, errorMessage = "something went wrong") {
 //   })
 //   .then(() => {
 //     currentImg.style.display = "none";
-//     return createImage("/Section11/img-2.jpg");
+//     return createImage("/Section16/img-2.jpg");
 //   })
 //   .then((img) => {
 //     currentImg = img;
@@ -290,6 +290,135 @@ const getJson = function (url, errorMessage = "something went wrong") {
 // };
 // get3Countries("bharat", "greenland", "china");
 
-const GITHUB_API = "https://api.github.com/users/bhavii1304";
-const user = fetch(GITHUB_API);
-console.log(user);
+// const GITHUB_API = "https://api.github.com/users/bhavii1304";
+// const user = fetch(GITHUB_API);
+// console.log(user);
+
+// --------- other promise combinators race all settled and any---------------
+// promise.race in this the 1st settled promise wins the race
+// (async function () {
+//   const res = await Promise.race([
+//     getJson(`https://restcountries.com/v3.1/name/italy`),
+//     getJson(`https://restcountries.com/v3.1/name/egypt`),
+//     getJson(`https://restcountries.com/v3.1/name/mexico`),
+//   ]);
+//   console.log(res[0]);
+// })();
+
+// const timeout = function (sec) {
+//   return new Promise(function (_, reject) {
+//     setTimeout(function () {
+//       reject(new Error("request took too long"));
+//     }, sec * 1000);
+//   });
+// };
+
+// Promise.race([
+//   getJson(`https://restcountries.com/v3.1/name/mexico`),
+//   timeout(0.001),
+// ])
+//   .then((res) => console.log(res[0]))
+//   .catch((err) => console.log(err));
+
+// Promise.allSettled([
+//   Promise.resolve("success"),
+//   Promise.reject("error"),
+//   Promise.resolve("Another success"),
+// ]).then((res) => console.log(res));
+
+// Promise.all([
+//   Promise.resolve("SUCCESS"),
+//   Promise.reject("ERROR"),
+//   Promise.resolve("ANOTHER SUCCESS"),
+// ])
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
+
+// Promise.any([
+//   Promise.resolve("SUCCESS"),
+//   Promise.reject("ERROR"),
+//   Promise.resolve("ANOTHER SUCCESS"),
+// ])
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
+
+// ----------challenge 3-----------------
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector(".images");
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+
+    img.addEventListener("load", function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener("error", function () {
+      reject(new Error("Image not found"));
+    });
+  });
+};
+
+let currentImg;
+
+// createImage('img/img-1.jpg')
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 1 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-2.jpg');
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 2 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//   })
+//   .catch(err => console.error(err));
+
+// PART 1
+const loadNPause = async function () {
+  try {
+    // Load image 1
+    let img = await createImage("Section16/img-1.jpg");
+    console.log("Image 1 loaded");
+    await wait(2);
+    img.style.display = "none";
+
+    // Load image 1
+    img = await createImage("Section16/img-2.jpg");
+    console.log("Image 2 loaded");
+    await wait(2);
+    img.style.display = "none";
+  } catch (err) {
+    console.error(err);
+  }
+};
+loadNPause();
+
+// PART 2
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async (img) => await createImage(img));
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+    imgsEl.forEach((img) => img.classList.add("parallel"));
+  } catch (err) {
+    console.error(err);
+  }
+};
+loadAll(["Section16/img-1.jpg", "Section16/img-2.jpg", "Section16/img-3.jpg"]);
